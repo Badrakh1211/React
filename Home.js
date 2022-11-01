9e3f46fbdcc55d5c8148456981b3ee6d053a63fe
Badrakh1211 <110883068+Badrakh1211@users.noreply.github.com>
2022-11-01 19:42:56 +0800

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Link} from 'react-router-dom'
+
+function Products() {  
+  return (
+    <div>
+        <p>Products</p>
+    </div>
+  );
+}
+
+export default Products;
